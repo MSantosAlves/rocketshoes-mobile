bb52f736c32734a2e692d3bca91a65d344785a58
Matheus Santos alves <matheussantosalves4@gmail.com>
2020-03-13 19:26:48 -0300

    Configurando Redux.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 
+import store from './store';
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="light-content" background="#fff" />
-      <Routes />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <StatusBar barStyle="light-content" background="#fff" />
+        <Routes />
+      </NavigationContainer>
+    </Provider>
   );
 }
