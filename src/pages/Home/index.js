f4debc8c590add78c90a333ec8cf5defbc71032a
Matheus Santos alves <matheussantosalves4@gmail.com>
2020-03-13 12:17:22 -0300

    Configurando json-server.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Home({ navigation }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function loadProducts() {
+      const response = api.get('/products');
+
+      setProducts(response);
+    }
+
+    loadProducts();
+  }, []);
+
   return (
     <>
-      <View>
-        <Text>Home</Text>
-      </View>
+      <Container />
     </>
   );
 }
