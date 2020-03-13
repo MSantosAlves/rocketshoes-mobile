9f33dd5b38ebc28ac2ab78ac71525babf5266291
Matheus Santos alves <matheussantosalves4@gmail.com>
2020-03-13 14:33:33 -0300

    Estilizando a página Home.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,64 @@
 import React, { useState, useEffect } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
-import { Container } from './styles';
+import { formatPrice } from '../../utils/formattedPrice';
+import '../../config/loadFont';
+
+import {
+  Container,
+  ProductsList,
+  ProductItem,
+  ProductImage,
+  Title,
+  Price,
+  AddProductButton,
+  ItemAmount,
+  ProductAmount,
+  AddProduct,
+} from './styles';
 
 export default function Home({ navigation }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     async function loadProducts() {
-      const response = api.get('/products');
-
-      setProducts(response);
+      const response = await api.get('/products');
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormatted: formatPrice(product.price),
+      }));
+      setProducts(data);
     }
 
     loadProducts();
   }, []);
 
-  return (
-    <>
-      <Container />
-    </>
-  );
+  function renderProductList() {
+    return (
+      <>
+        <ProductsList
+          data={products}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <ProductItem key={item.id}>
+              <ProductImage source={{ uri: item.image }} />
+              <Title>{item.title}</Title>
+              <Price>{item.priceFormatted}</Price>
+
+              <AddProductButton>
+                <ProductAmount>
+                  <Icon name="add-shopping-cart" color="#FFF" size={20} />
+                  <ItemAmount>0</ItemAmount>
+                </ProductAmount>
+                <AddProduct>ADICIONAR</AddProduct>
+              </AddProductButton>
+            </ProductItem>
+          )}
+        />
+      </>
+    );
+  }
+
+  return <Container>{renderProductList()}</Container>;
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,3 +1,67 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+import { darken } from 'polished';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  background: #191920;
+  align-items: center;
+`;
+
+export const ProductsList = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})``;
+
+export const ProductItem = styled.View`
+  background: #fff;
+  padding: 10px;
+  margin: 15px;
+  border-radius: 4px;
+  width: 300px;
+`;
+
+export const ProductImage = styled.Image`
+  width: 280px;
+  height: 280px;
+`;
+
+export const Title = styled.Text`
+  color: #333;
+  font-size: 16px;
+`;
+
+export const Price = styled.Text`
+  font-size: 21px;
+  color: #333;
+  font-weight: bold;
+  margin: 5px 0 20px;
+`;
+
+export const AddProductButton = styled(RectButton)`
+  background: #7159c1;
+  align-items: center;
+  flex-direction: row;
+  border-radius: 4px;
+  margin-top: auto;
+  width: 280px;
+`;
+
+export const ProductAmount = styled.View`
+  padding: 12px;
+  background: ${darken(0.03, '#7159c1')};
+  align-items: center;
+  flex-direction: row;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+`;
+
+export const ItemAmount = styled.Text`
+  color: #eee;
+  margin-left: 5px;
+`;
+
+export const AddProduct = styled.Text`
+  flex: 1;
+  text-align: center;
+  color: #eee;
+  font-weight: bold;
+`;
